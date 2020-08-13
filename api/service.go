2afd11da1e009a564968e25dae5ed2dbe9177329
shoma-www <shumai.1215.2018@gmail.com>
2020-08-14 06:55:58 +0900

    実行環境としてdockerファイルを追加

--- api/service.go
+++ api/service.go
@@ -19,7 +19,7 @@ func NewHealthCheckService() HealthCheckService {
 
 // Grpc GrpcのサーバーにHealthCheckを実施
 func (hs HealthCheckService) Grpc(ctx context.Context) error {
-	conn, err := grpc.Dial("127.0.0.1:50051", grpc.WithInsecure())
+	conn, err := grpc.Dial("grpc:50051", grpc.WithInsecure())
 	if err != nil {
 		return err
 	}
