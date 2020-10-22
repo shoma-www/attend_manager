cbf6c7481d7de90d732d2ef19ceb781b28f62b19
shoma-www <shumai.1215.2018@gmail.com>
2020-10-23 02:31:38 +0900

    implement session

--- api/service/session.go
+++ api/service/session.go
@@ -1,8 +1,14 @@
 package service
 
 import (
+	"bytes"
 	"context"
+	"encoding/gob"
+	"encoding/hex"
+	"time"
 
+	"github.com/go-redis/redis"
+	"github.com/gorilla/securecookie"
 	"github.com/shoma-www/attend_manager/api/entity"
 )
 
@@ -13,7 +19,7 @@ type Session interface {
 	Destoroy(ctx context.Context, s entity.Session) error
 }
 
-// const lifetime = 60 * time.Hour
+const lifetime = 60 * time.Hour
 
 type RedisSession struct {
 	addr     string
@@ -29,14 +35,59 @@ func NewRedisSession(addr, password string, db int) Session {
 	}
 }
 
-func (RedisSession) Start(ctx context.Context, values entity.Store) (*entity.Session, error) {
-	return nil, nil
+func (rs RedisSession) Start(ctx context.Context, values entity.Store) (*entity.Session, error) {
+	client := rs.newClient()
+	defer client.Close()
+
+	key := hex.EncodeToString(securecookie.GenerateRandomKey(64))
+	b := new(bytes.Buffer)
+	e := gob.NewEncoder(b)
+	if err := e.Encode(values); err != nil {
+		return nil, err
+	}
+
+	if err := client.Set(key, b.Bytes(), lifetime).Err(); err != nil {
+		return nil, err
+	}
+	return &entity.Session{
+		ID:     entity.SessionID(key),
+		Values: values,
+	}, nil
 }
 
-func (RedisSession) Get(ctx context.Context, id entity.SessionID) (*entity.Session, error) {
-	return nil, nil
+func (rs RedisSession) Get(ctx context.Context, id entity.SessionID) (*entity.Session, error) {
+	client := rs.newClient()
+	defer client.Close()
+
+	b, err := client.Get(string(id)).Result()
+	if err != nil {
+		return nil, err
+	}
+	buf := bytes.NewBuffer([]byte(b))
+	values := make(entity.Store)
+	d := gob.NewDecoder(buf)
+	if err := d.Decode(&values); err != nil {
+		return nil, err
+	}
+	return &entity.Session{
+		ID:     id,
+		Values: values,
+	}, nil
 }
 
-func (RedisSession) Destoroy(ctx context.Context, s entity.Session) error {
+func (rs RedisSession) Destoroy(ctx context.Context, s entity.Session) error {
+	client := rs.newClient()
+	defer client.Close()
+	if err := client.Del(string(s.ID)).Err(); err != nil {
+		return err
+	}
 	return nil
+}
+
+func (rs RedisSession) newClient() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     rs.addr,
+		Password: rs.password,
+		DB:       rs.db,
+	})
 }
